7d242cb1d25696ddde24e566ef34c5bcac9940f1
hagiwara <hagiwara-ky@rittor-music.co.jp>
2020-02-19 12:46:44 +0900

    とりあえず動いてhtml出力できる

--- src/script/main.py
+++ src/script/main.py
@@ -2,6 +2,7 @@ import csv
 import json
 import re
 import datetime
+from jinja2 import Template, Environment, FileSystemLoader
 
 # if __name__ == '__main__':
 
@@ -58,6 +59,18 @@ default_data['sale']['period'] = {
 }
 print(default_data)
 
-savepath = '../../src/resource/default/test_client/' + 'default_extend_{}.json'.format(now_str)
-with open(savepath, 'w', encoding='utf-8') as outfile:
+default_savepath = '../../src/resource/default/test_client/' + 'default_extend_{}.json'.format(now_str)
+with open(default_savepath, 'w', encoding='utf-8') as outfile:
     json.dump(default_data, outfile,ensure_ascii=False)
+
+
+
+# 今回の分のdefault.jsonを読み込んで、outputのhtmlを生成
+
+env = Environment(loader=FileSystemLoader('../', encoding='utf_8'))
+tpl = env.get_template('./template/PC/general_template.html')
+render = tpl.render(default_data)
+
+output_savepath = '../../src/resource/default/test_client/' + 'output_{}.html'.format(now_str)
+with open(output_savepath, 'w', encoding='utf_8') as stream:
+        stream.write(render)
